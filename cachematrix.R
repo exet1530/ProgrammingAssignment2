091c5c59a1dbb79ab3e5e1b3b5d7532ece521277
Nic Scott <exet1530@gmail.com>
2020-09-12 16:49:00 +1000

    Replicated makeVector/cachemean example but using solve so that inverse of a matrix is returned instead of mean of a vector

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Create an improved matrix object that can store it's own inverse,
+## and will return this stored value rather than recalculating it if
+## the matrix has not changed
 
-## Write a short comment describing this function
+## Create a special matrix object that can store it's own inverse
+## if it has already been calculated. It has functions set, get
+## which set or get the matrix itself and setinverse, getinverse
+## which set or get the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+   m <- NULL
+   set <- function(y) {
+      x <<- y
+      m <<- NULL
+   }
+   get <- function () x
+   setinverse <- function(inverse) m <<- inverse
+   getinverse <- function () m
+   list(set = set, get = get, setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Calculate the inverse of a matrix. If the inverse of that
+## matrix has already been calculated return the stored result
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+   m <- x$getinverse()
+   if (!is.null(m)) {
+         message("getting cached data")
+         return(m)
+   }
+   data <- x$get()
+   m <- solve(data,...)
+   x$setinverse(m)
+   m
 }
